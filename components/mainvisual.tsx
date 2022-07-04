7a36c7037a506b1cc19133068b5cc58eefc727c0
CreateWebSiteTeam <yourgitemail@example.com>
2022-07-05 02:04:19 +0900

    動画修正

--- components/mainvisual.tsx
+++ components/mainvisual.tsx
@@ -4,14 +4,11 @@ import Image from "next/image";
 export const MainVisual:React.FC = () => {
   return(
     <div className="h-screen w-full relative">
-      <video
-      autoPlay
-      loop
-      muted
+      <video autoPlay muted loop
       playsInline
       className="absolute w-full h-full object-cover"
+      src="/assets/top-back.mp4"
       >
-        <source src="/assets/top-back.mp4" type="video/mp4; codecs=hvc1"/>
       </video>
       <ArrowDown/>
     </div>
